b38234265373754119585707cd02378cda879a24
altynaykemelova <kemelovaaltynay24@gmail.com>
2024-05-05 19:42:15 +0600

    home fragment

--- app/src/main/java/com/example/flowershop/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/flowershop/ui/home/HomeFragment.java
@@ -7,26 +7,58 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
+import com.airbnb.lottie.LottieAnimationView;
+import com.example.flowershop.R;
 import com.example.flowershop.databinding.FragmentHomeBinding;
+import com.example.flowershop.models.CategoryModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
-
+    List<CategoryModel> list_category = new ArrayList<>();
+    CategoryAdapter adapter;
+    LottieAnimationView lotty_flowers;
+    NavController navController;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
+
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        lotty_flowers = binding.lottyFlowers;
+        lotty_flowers.setAnimation(R.raw.flowers2);
+//2,3(so so),6
+        createList();
+        adapter = new CategoryAdapter();
+        adapter.setMain_list(list_category);
+        binding.rvCatalogCategory.setAdapter(adapter);
         return root;
+    }
+    private void createList() {
+        list_category.add(new CategoryModel("Houseplants", R.drawable.hauseplants));
+        list_category.add(new CategoryModel("Bouquets", R.drawable.bouqet));
+        list_category.add(new CategoryModel("Wedding", R.drawable.wedding));
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding.btnLogin.setOnClickListener(v->{
+            navController = Navigation.findNavController(requireActivity(),
+                    R.id.nav_host);
+            navController.navigate(R.id.action_navigation_home_to_authFragment);
+        });
     }
 
     @Override
